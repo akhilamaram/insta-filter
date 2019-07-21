537c68cfd85b46f1b5a681d26acfe0a9bc675c5f
akhilamaram <48825743+akhilamaram@users.noreply.github.com>
2019-07-21 10:33:27 +0530

    firstcommit

---
+++ instafilter.py
@@ -0,0 +1,23 @@
+#Import Image from PIL
+from PIL import Image
+
+#Load Images into Objects
+print(os.listdir()) 
+base_img= Image.open("portrait.jpg")
+img_filter=Image.open("filter3.jpg")
+
+#Set O/P Image size
+size=(750,750)
+
+#Resize all the images to o/p size
+base_img=base_img.resize(size)
+img_filter=img_filter.resize(size)
+
+#split each image into RGB vectors
+r,g,b=base_img.split()
+R,G,B=img_filter.split()
+
+#Merge RGB vectors from digfrent images
+im=Image.merge("RGB", (r,G,b))
+im.show()
+im.save('1_merged.jpg')
